6a47d94ea492e2bd9ee2d5fc93fdc129b83b6113
John A Clarke <jaclarke@mit.edu>
2016-12-06 17:50:46 -0500

    predictions working: result still sparse. need to create more predictive feature vectors or come up with procedure to deduce words from softmax with dozens of equal entries

--- embedding.py
+++ embedding.py
@@ -2,23 +2,24 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction import DictVectorizer
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
+from math import exp, floor
 
 class Embedding(object):
 
         def __init__(self,data_file,train_partition):
 		
-                x, y = get_data(data_file)
-                self.xtrain = x[:len(x)*train_partition]
-                self.xtest = x[len(x)*train_partition:]
-                self.ytrain = y[:len(y)*train_partition]
-                self.ytest = y[len(y)*train_partition:]
+                x, y = self.get_data(data_file)
+		self.xtrain = x[:int(floor(len(x)*train_partition))]
+                self.xtest = x[int(floor(len(x)*train_partition)):]
+                self.ytrain = y[:int(floor(len(y)*train_partition))]
+                self.ytest = y[int(floor(len(y)*train_partition)):]
+		
+        
+	def set_vocab(self,vocab):
+		self.vocab = vocab
 
-                self.model = None
-                self.is_trained = False
-                self.vocab_size = 0
-                
         # todo: modify to allow concatenation of various data files
 	def get_data(self,file):
 		
@@ -42,10 +43,12 @@ class Embedding(object):
 		for headline in headlines_annotated:
 			headline = headline.split(' ')
 			for word in headline:
-                            if word not in headline:
+                            if word not in vocab:
 			        vocab[word] = index
                                 index += 1
+		self.set_vocab(vocab)
                 vector_length = len(vocab)
+	   	self.vocab_size = vector_length
                 for headline in headlines_annotated:
                     output = np.zeros((vector_length))
                     headline = headline.split(' ')
@@ -55,47 +58,57 @@ class Embedding(object):
                         else:
                             output[vocab[word]] += 1
                     # apply softmax
-                    output = map(lambda x: e**x, output)
+                    output = np.array(map(lambda x: exp(x), output))
                     output = output / sum(output)
                     y.append(output)
-                
+                y = np.array(y)
+
                 # create input x
                 x = []
                 v = DictVectorizer()
                 for index, row in data.iterrows():
-                    input = {}
-                    for name in column:
-                        input[name] = row[name]
-                    x.append(input)
-                x = v.fit_transform(x)
-                
-                self.vocab_size = len(vocab)
-                self.is_trained = False
+                    inp = {}
+                    for name in columns:
+                        inp[name] = row[name]
+                    x.append(inp)
+                x = v.fit_transform(x).toarray()
                 return (x,y)
-	
-        def train(self, learning_rate = 0.01, epochs=20, batch_size=256):
-            input_size = len(self.x[0])
-            if not self.is_trained:
-                model = Sequential()
+
+	def train(self, loss, learning_rate = 0.01, epochs=20, batch_size=256):
+	   	input_size = len(self.xtrain[0])
+	    	self.vocab_size = len(self.vocab)
+            	model = Sequential()
                 model.add(Dense(2*input_size,input_dim=input_size))
                 model.add(Activation('relu'))
                 model.add(Dropout(0.25))
                 model.add(Dense(self.vocab_size))
                 model.add(Activation('softmax'))
                 sgd = SGD(lr=learning_rate)
-                model.compile('mean_squared_error', optimizer=sgd)
+                model.compile(loss=loss, optimizer=sgd)
                 model.fit(self.xtrain, self.ytrain, nb_epoch = epochs, batch_size=batch_size)
-
-                self.model = model
+		self.model = model
                 self.is_trained = True
-            else:
-                print 'the model is already trained'
         
         def predict(self):
-            if self.is_trained:
-                print self.model.predict_classes(self.xtest)
-                print self.model.predict_proba(self.xtest)
-            else:
-                print 'the model has not been trained'
+                classes = self.model.predict_classes(self.xtest)
+		proba = self.model.predict_proba(self.xtest)
+		return (classes,proba)
+	
+	def normalize(self,predictions):
+		norm = []
+		for p in predictions:
+			avg = np.average(p)
+			norm.append(map(lambda x: x if x >= avg else 0, p))
+		norm = np.array(norm)
+		return norm
 
-e = Embedding()
+
+
+f = 'nfl_game_stats_2016_annotated_clean.csv'
+partition = 0.70
+e = Embedding(f,partition)
+e.train('categorical_crossentropy')
+classes, proba = e.predict()
+proba_norm = e.normalize(proba)
+for p in proba_norm:
+	print p
